bbc40efe31f7b521f1d1a83c3bace3dbea503a80
Witold Kupś <witold.kups@interlan.pl>
2020-05-03 15:05:11 +0200

    Add SatisfactionLevelsUpdate interface

---
+++ lib/logic/limited-rationality/satisfaction-levels/satisfaction-levels-update.go
@@ -0,0 +1,53 @@
+package satisfaction_levels
+
+import (
+	"fmt"
+	"github.com/Azbesciak/RealDecisionMaker/lib/model"
+	"github.com/Azbesciak/RealDecisionMaker/lib/utils"
+	"sort"
+)
+
+type ParamsAddition = interface{}
+
+type CriterionAdder interface {
+	OnCriterionAdded(
+		criterion *model.Criterion,
+		previousRankedCriteria *model.Criteria,
+		params SatisfactionLevels,
+		generator utils.ValueGenerator,
+	) ParamsAddition
+}
+
+type SatisfactionParamsMerger interface {
+	Merge(params SatisfactionLevels, addition ParamsAddition) SatisfactionLevels
+}
+
+type CriterionRemover interface {
+	OnCriteriaRemoved(leftCriteria *model.Criteria, params SatisfactionLevels) SatisfactionLevels
+}
+
+type SatisfactionLevelsUpdateListener interface {
+	SatisfactionParamsSource
+	CriterionAdder
+	CriterionRemover
+	SatisfactionParamsMerger
+}
+type ListenersMap = map[string]SatisfactionLevelsUpdateListener
+
+type SatisfactionLevelsUpdateListeners struct {
+	Listeners ListenersMap
+}
+
+func (sl *SatisfactionLevelsUpdateListeners) Fetch(listenerName string) *SatisfactionLevelsUpdateListener {
+	fun, ok := (sl.Listeners)[listenerName]
+	if !ok {
+		keys := make([]string, 0)
+		for k := range sl.Listeners {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		panic(fmt.Errorf("satisfaction levels update listener for '%s' not found, available are '%s'", listenerName, keys))
+	}
+	listener := fun.(SatisfactionLevelsUpdateListener)
+	return &listener
+}
